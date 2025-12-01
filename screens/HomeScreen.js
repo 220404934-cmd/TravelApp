1143002205c22540351ee934ac46df8b023a6e5a
sara12345batal <Saramagayri@gmail.com>
2025-12-01 19:21:15 +0300

    feat: Home ve Details ekranları ile stack navigator uygulaması (Bölüm 2)

---
+++ screens/HomeScreen.js
@@ -0,0 +1,26 @@
+import { View, Text, Button, StyleSheet } from "react-native";
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Home Screen</Text>
+
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 20
+  },
+  title: {
+    fontSize: 22,
+  },
+});
